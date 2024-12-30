1e998cb8018712df6faace2a061282614b8bb236
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-30 10:13:25 +0100

    created and added notations

---
+++ src/main/java/org/example/cvfeedbackbackend/entity/CV.java
@@ -0,0 +1,34 @@
+package org.example.cvfeedbackbackend.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CV {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String workExperience;
+    @Column(columnDefinition = "TEXT")
+    private String education;
+    @Column(columnDefinition = "TEXT")
+    private String skills;
+    private String feedback;
+
+
+    public CV(String name, String workExperience, String education, String skills) {
+        this.name = name;
+        this.workExperience = workExperience;
+        this.education = education;
+        this.skills = skills;
+    }
+}
