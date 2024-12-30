24413a6c2423d1f90f4e0078019e748eacc00132
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-30 13:33:45 +0100

    created and implemented configuration classes

---
+++ src/main/java/org/example/cvfeedbackbackend/config/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package org.example.cvfeedbackbackend.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An error occurred");
+    }
+}
+
