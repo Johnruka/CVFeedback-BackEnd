0ebc699104ebb611a208aa206cfaa2c713c88482
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-30 13:34:22 +0100

    modified

--- src/main/java/org/example/cvfeedbackbackend/controller/CVFeedbackController.java
+++ src/main/java/org/example/cvfeedbackbackend/controller/CVFeedbackController.java
@@ -5,13 +5,11 @@ import org.example.cvfeedbackbackend.repository.CVRepository;
 import org.example.cvfeedbackbackend.service.AIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cv-feedback/feedback")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CVFeedbackController {
 
     @Autowired
