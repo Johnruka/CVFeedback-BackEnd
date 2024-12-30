f39fc96cb205dabec733fdde611a0e6b9c2d20b9
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-12-30 09:59:31 +0100

    Initial commit

---
+++ src/main/java/org/example/cvfeedbackbackend/CvFeedbackBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.cvfeedbackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvFeedbackBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvFeedbackBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cvfeedbackbackend/CvFeedbackBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cvfeedbackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvFeedbackBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
